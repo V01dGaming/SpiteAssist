837b0a1f018db43e21a714b5ebf4c1714ccbf982
V01d_P4r4d0x <43250390+V01dGaming@users.noreply.github.com>
2023-08-09 19:51:07 -0500

    Update Run.py
    Changed wake word to "Zero" because "Spite" wasn't being picked up very well.
    
    Added a beep so you know when it picks up your voice so you can use it in the background now... don't know why it took this long to think of that addition.

--- Run.py
+++ Run.py
@@ -4,6 +4,7 @@ import webbrowser as web
 import secrets as sec
 import os
 import sys
+import winsound
 
 Steamoptions = ["open steam", "start steam", "run steam", "please open steam", "please start steam", "please run steam", "open steam please", "start steam please", "run steam please"]
 Discordoptions = ["open discord", "start discord", "run discord", "please open discord", "please start discord", "please run discord", "open discord please", "start discord please", "run discord please"]
@@ -11,7 +12,8 @@ Restartoptions = ["restart", "please restart", "restart please"]
 Crunchyrolloptions = ["open crunchyroll", "open my anime", "open anime", "please open crunchyroll", "please open my anime", "please open anime", "open crunchyroll please", "open my anime please", "open anime please"]
 Internetoptions = ["open internet", "open the internet", "open chrome", "open the browser", "open the web", "open browser", "open web", "please open internet", "please open the internet", "please open chrome", "please open the browser", "please open the web", "please open browser", "please open web", "open internet please", "open the internet please", "open chrome please", "open the browser please", "open the web please", "open browser please", "open web please"]
 Shutdownoptions = ["shut down", "shutdown", "power off", "turn off", "power down", "terminate", "shut down please", "shutdown please", "power off please", "turn off please", "power down please", "terminate please", "please shut down", "please shutdown", "please power off", "please turn off", "please power down", "please terminate"]
-Wakeword = ["spite", "hey spite"]
+Nevermind = ["nevermind", "never mind"]
+Wakeword = ["zero", "hey zero"]
 
 #Phrases - 1 phrase provided by human, the rest provided by ChatGPT
 openanime = ["I hope you enjoy your anime!", "May you have a delightful time watching your anime!", "Wishing you a wonderful experience with your anime!", "I hope your anime brings you immense enjoyment!", "May your anime viewing be filled with joy and satisfaction!"]
@@ -21,6 +23,10 @@ shutdownpls = ["Goodbye, I hope to see you soon!", "Farewell, looking forward to
 
 web.register("chrome", None, web.BackgroundBrowser("C:\Program Files\Google\Chrome\Application\chrome.exe"))
 
+def beep():
+    frequency = 2000
+    duration = 300
+    winsound.Beep(frequency, duration)
 
 def clr1():
     if(os.name == 'posix'):
@@ -44,6 +50,7 @@ def speech_recognition1():
             if txt1 in Wakeword:
                 print("Wake word detected: ", txt1)
                 t.sleep(1)
+                beep()
                 speech_recognition2()
         except sr.UnknownValueError:
             print("Unable to understand speech")
@@ -98,6 +105,9 @@ def speech_recognition2():
                 print(P)
                 t.sleep(2)
                 quit()
+            elif txt in Nevermind:
+                t.sleep(2)
+                clr()
         except sr.UnknownValueError:
             print("Unable to understand speech")
             t.sleep(2)
